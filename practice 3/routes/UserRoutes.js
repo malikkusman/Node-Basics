8322dc6bc220ccb991902c9423824d9e4642cb42
Muhammad Usman Asghar <42khan0@gmail.com>
2023-10-18 17:11:35 +0500

    Upate UserModels and UserControllers

--- practice 3/routes/UserRoutes.js
+++ practice 3/routes/UserRoutes.js
@@ -4,5 +4,8 @@ const route = express.Router();
 const usercontroller = require("../controller/UserController");
 
 route.post("/users", usercontroller.AddUser);
+route.get("/users", usercontroller.getAllUsers);
+route.put("/users/:id", usercontroller.updateUsers);
+route.delete("/users/:id", usercontroller.deleteUsers);
 
 module.exports = route;
