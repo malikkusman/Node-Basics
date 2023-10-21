9d61d0f3e977916bc685bc053426e3d978d222bd
Muhammad Usman Asghar <42khan0@gmail.com>
2023-10-21 11:15:18 +0500

    Resolve Issues

--- practice 3/controller/LoginController.js
+++ practice 3/controller/LoginController.js
@@ -3,12 +3,9 @@ const usermodel = require("../models/UserModels");
 async function LoginUser(req, res) {
   try {
     const { Username, Password } = req.body;
-    const loginuser = await usermodel.findOne({ Username });
-    if (!loginuser || loginuser.Password !== Password) {
-      return res.status(404).json({ error: "Invalid Username or Password " });
-    } else {
-      res.status(200).json({ loginuser });
-    }
+    const loginuser = await usermodel.findOne({ Username, Password });
+
+    res.status(200).json({ loginuser });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
