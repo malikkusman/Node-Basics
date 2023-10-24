86a49ed89639ed97dce28f290af89539593aee39
Muhammad Usman Asghar <42khan0@gmail.com>
2023-10-24 22:14:19 +0500

    update login function

--- practice 4/controller/LoginController.js
+++ practice 4/controller/LoginController.js
@@ -1,11 +1,24 @@
 const usermodel = require("../models/UserModels");
 
-async function LoginUser(req, res) {
+async function LoginUser(req, res, next) {
   try {
     const { Username, Password } = req.body;
     const loginuser = await usermodel.findOne({ Username, Password });
+    if (!loginuser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
 
-    res.status(200).json({ loginuser });
+    var token = generteLoginToken(loginuser);
+    return res.status(200).json({
+      message: "Logged in successfully",
+      UserName: Username,
+      FirstName: loginuser.FirstName,
+      userId: loginuser.id,
+      token: token,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
