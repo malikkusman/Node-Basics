b71442126cdd7b825763b0a23d40980fc5bb2eb7
Muhammad Usman Asghar <42khan0@gmail.com>
2023-11-29 15:28:46 +0500

    delete files

--- practice 2/routes/CardRouttes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const cardcontroller = require("../controllers/CardController");
-
-router.post("/Card", cardcontroller.AddCard);
-
-module.exports = router;
